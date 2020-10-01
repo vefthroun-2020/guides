4585bb5aaf41a14aaefaf71cffd64624f2c976ec
Smari <ellertsmari@gmail.com>
2020-10-01 10:09:48 +0000

    arrays

--- dataTypes.js
+++ dataTypes.js
@@ -1,1 +1,25 @@
-console.log("test")
+//console.log("test")
+
+var myBoolean = true;
+var myEmptyString = "";
+var myString1 = "some text1"
+var myString2 = 'some text2'
+var myString3 = `some text3
+many lines
+
+`
+var myNumbers = 3;
+
+//console.log(myString2)
+
+//arrays
+
+var myArray = [];
+var myGradesArray = [8, 5, 6, 7, true, 8, 10];
+
+//console.log("myArray", myArray);
+//console.log("myGradesArray", myGradesArray);
+console.log("the last from myGradesArray", myGradesArray[myGradesArray.length-1])
+
+//objects
+
